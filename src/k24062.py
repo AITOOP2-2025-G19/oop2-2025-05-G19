34ecc4a84d7c90a618542c0d42527af360f0eea4
Haruto-Koshimizu <ko.haru414@icloud.com>
2025-10-30 15:14:31 +0900

    Update k24062.py

--- src/k24062.py
+++ src/k24062.py
@@ -10,9 +10,7 @@ def lecture05_01():
 
     # 画像をローカル変数に保存
     google_img : cv2.Mat = cv2.imread('images/google.png')
-    capture_img : cv2.Mat = cv2.imread('output_images/camera_capture.png') # 動作テスト用なので提出時にこの行を消すこと
-
-    # capture_img : cv2.Mat = "implement me"
+    capture_img : cv2.Mat = app.get_img()
 
     g_hight, g_width, g_channel = google_img.shape
     c_hight, c_width, c_channel = capture_img.shape
